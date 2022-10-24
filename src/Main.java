01347e3e29a5b43d91615e846ddddba02d015960
MehmetKurmaz <111184509+MehmetKurmaz@users.noreply.github.com>
2022-10-24 22:02:14 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         int girilenSayi, adet = 0, toplam = 0;
         double ortalama;
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Lutfen Bir Sayi Giriniz : ");
-        girilenSayi = sc.nextInt();
+        girilenSayi = input.nextInt();
 
         for(int i = 0; i < girilenSayi; i++){
             if(i%3==0 && i%4==0){
@@ -18,7 +18,7 @@ public class Main {
 
         ortalama = toplam/adet;
         System.out.print("0'dan girilen sayiya kadar olan sayilardan 3 ve 4'e tam bolunen sayilarin ortalamasi  :"+ortalama);
-        sc.close();
+        input.close();
 
 
 
